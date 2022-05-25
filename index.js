f90baf1847922d637980e228e3339defdf106f7f
MANZI MUVUNYI Toussaint <toussaintmanzi006@gmail.com>
2022-05-25 14:16:20 +0200

    adding toolkit to social media and animation to burger

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const burger = document.querySelector('.social-media .burger');
 const nav = document.querySelector('.sidebar ul');
-
+const navLinks = document.querySelectorAll(".sidebar ul li");
 // console.log("this is burger: ", burger);
 // console.log("nav: ", nav);
 
 burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
 });
+
