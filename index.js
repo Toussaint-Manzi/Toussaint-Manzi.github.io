14dab246ba40f650dab66e2331030e58c360b38c
MANZI MUVUNYI Toussaint <toussaintmanzi006@gmail.com>
2022-05-29 11:56:33 +0200

    removing unneccessary comments

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const burger = document.querySelector('.social-media .burger');
 const nav = document.querySelector('.sidebar ul');
 const navLinks = document.querySelectorAll(".sidebar ul li");
-// console.log("this is burger: ", burger);
-// console.log("nav: ", nav);
 
 burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
