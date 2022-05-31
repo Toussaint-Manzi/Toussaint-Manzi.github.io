b97c52cbe073d6d99812bd24d2261d8420e1912b
MANZI MUVUNYI Toussaint <toussaintmanzi006@gmail.com>
2022-05-31 10:04:32 +0200

    add some changes on responsiveness

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const burger = document.querySelector('.social-media .burger');
 const nav = document.querySelector('.sidebar ul');
-const navLinks = document.querySelectorAll(".sidebar ul li");
+// const navLinks = document.querySelectorAll(".sidebar ul li");
 
 burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
     burger.classList.toggle('toggle');
 });
+
+// navLinks.addEventListener('click')
 
